74d5507ed7598513cf881afe158cd5c677ce8c5e
bluefog <bluefog@bluefogui-MacBookPro.local>
2025-04-20 17:25:08 +0900

    v3. after SharedPreferences

--- lib/game/components/flame/room_game.dart
+++ lib/game/components/flame/room_game.dart
@@ -5,27 +5,23 @@ import 'package:flame/input.dart';
 import '../../scenes/intro_scene.dart';
 import 'girl_sprite.dart';
 import 'jordy_sprite.dart';
-import 'dart:ui'; // ✅ Color를 위한 올바른 import
+import 'dart:ui';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RoomGame extends FlameGame with HasCollisionDetection {
   late GirlSprite girl;
   late JordySprite jordy;
-  late IntroScene _introScene;
 
   @override
   Color backgroundColor() => const Color(0xFF111111);
 
   @override
   Future<void> onLoad() async {
-    print("🧱 RoomGame size = \$size");
-    print("📱 canvasSize = \$canvasSize");
-    print("📐 gameRef.size: \$size");
-    print("🔍 devicePixelRatio = \${window.devicePixelRatio}");
-
     await images.loadAll([
       'background.png',
       'girl_walk.png',
       'jordy_idle.png',
+      'jordy_closeup.png',
     ]);
 
     // 방 배경
@@ -43,8 +39,10 @@ class RoomGame extends FlameGame with HasCollisionDetection {
     jordy = JordySprite(position: Vector2(250, 400));
     add(jordy);
 
-    // 인트로 대사
-    _introScene = IntroScene();
-    await add(_introScene);
+    final prefs = await SharedPreferences.getInstance();
+    final introIndex = prefs.getInt('intro_dialogue_index') ?? 0;
+    if (introIndex < 9999) {
+      add(IntroScene());
+    }
   }
 }
